4a017548d0ae9f41af2858c9eae847f595f80562
aryoferdyan <63910862+aryoferdyan@users.noreply.github.com>
2021-10-07 16:55:35 +0700

    update

---
+++ ProjectAdvancedOperasi/src/projectadvancedoperasi/Operasi.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectadvancedoperasi;
+
+/**
+ *
+ * @author Asus
+ */
+public class Operasi {
+    
+    int jumlahkan(int a, int b){
+        return (a+b);
+    }
+    	
+    int jumlahkan(int a, int b, int c){
+        return (a+b+c);
+    }
+    
+    double jumlahkan(double a, double b){
+        return (a+b);
+    }
+    
+    double jumlahkan(double a, double b, double c){
+        return (a+b+c);
+    }
+    
+    //PERKALIAN
+    double kalikan(double x, double y){
+        return (x*y);
+    }
+    
+    double kalikan(double x, double y, double z){
+        return (x*y*z);
+    }
+    
+    int kalikan(int x, int y){
+        return (x*y);
+    }
+    
+    int kalikan(int x, int y, int z){
+        return (x*y*z);
+    }
+}
